7b4a385cef96c381eb89983c1422c7058f3011dd
Bruno Rodrigues Silva <brsilva@leega.local>
2021-09-15 00:39:04 -0300

    Atualizado

--- terraform/bigquery/providers.tf
+++
@@ -1,9 +0,0 @@
-provider "google" {
-  version = "3.32.0"
-
-  credentials = file("terraform_account.json")
-
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
-}
--- terraform/composer/providers.tf
+++
@@ -1,6 +0,0 @@
-provider "google" {
-  credentials = "terraform_account.json"
-  project = var.project_id
-  region = var.region
-  zone    = var.zone
-}
